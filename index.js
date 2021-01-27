efb9aab821a0f20e8ccb1f638ff1611af9fd1f92
psalmex <samexbaba4all@gmail.com>
2021-01-27 13:53:40 +0100

    perimeter worked

--- index.js
+++ index.js
@@ -3,29 +3,28 @@ const radius = document.getElementById('radius-peri');
 const squareLenght = document.getElementById('sqr-lenght');
 const rectangleLenght = document.getElementById('rec-lenght');
 const rectangleWidth = document.getElementById('rec-width');
+const cylinderRadius = document.getElementById('cyl-radius');
+const cylinderHeight = document.getElementById('cyl-height');
 const para = document.getElementById('para');
 const paraSquare = document.getElementById('para-square');
 const paraRectangle = document.getElementById('para-rec');
+const paraCylinder = document.getElementById('para-cyl');
 const calc = document.querySelector('.calc');
 const calcSquare = document.querySelector('.calc-square');
 const calcRec = document.querySelector('.calc-rec');
+const calcCylinder = document.querySelector('.calc-cylinder');
 
 // Perimeter of a Circle
 
-// function circumference(rad, pi = 3.1415926535) {
-//   let result = 2 * pi * rad;
-//   para.innerHTML = `<h1> ${result}</h1>`;
-// }
+function circumference(radius) {
+  let result = 2 * 3.1415926535 * radius;
+  para.innerHTML = `<h1>${result}</h1>`;
+}
 
 calc.addEventListener('click', e => {
   e.preventDefault();
-  let circle1 = new circle(radius);
-  let result = circle1.circumference();
-  paraSquare.innerHTML = `<h1>${result}</h1>`;
 
-  
-
-  // circumference(radius.value);
+   circumference(radius.value);
 });
 
 // Perimeter of a square
@@ -55,3 +54,16 @@ calcRec.addEventListener('click', e => {
   rectanglePerimeter(rectangleLenght.value, rectangleWidth.value);
 });
 
+
+//Perimeter of a Cylinder
+function cylinderPerimeter(rad, height, pi = 3.1415926535) {
+  let result = 2 * pi * rad * height;
+  paraCylinder.innerHTML = `<h1> ${result}</h1>`;
+}
+
+calcCylinder.addEventListener('click', e => {
+  e.preventDefault();
+
+// console.log(paraCylinder)
+  cylinderPerimeter(cylinderRadius.value, cylinderHeight.value);
+});
